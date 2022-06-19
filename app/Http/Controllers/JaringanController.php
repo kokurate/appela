0a4be602b949d44294eae55e8c3175127434cb6a
kokurate <ericksanger66@gmail.com>
2022-06-20 07:34:57 +0800

    Using Bootstrap for the default pagination. Illuminate\Pagination\Paginator

--- app/Http/Controllers/JaringanController.php
+++ app/Http/Controllers/JaringanController.php
@@ -10,14 +10,14 @@ class JaringanController extends Controller
 {
     public function index(Request $request){
      // Kalo mo ubah paginasi ubaj juga variabelnya like this 15->all
-     $pagination = 2;
+     $pagination = 15;
 
 
         return view('_jaringan.index',[
           'title' => 'Ini halaman jaringan setelah login',
           'pengaduan' => Pengaduan::where('status','Pengaduan Sedang Diverifikasi')
                                   ->where('tujuan_id','1')
-                                  ->paginate(2)->withQueryString(), 
+                                  ->paginate(15)->withQueryString(), 
         ])->with('i', ($request->input('page', 1) - 1) * $pagination); // code for paginate     ;
       }
 
